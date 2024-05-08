d9758699dc4c4a5a1e1460456c7bdb54bd4a7c4b
Jacek programmer <jacek301@gmail.com>
2024-05-08 15:16:38 +0200

    Return Negative Kata Start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,11 +1,11 @@
 import codewars_test as test
-from solution import open_or_senior
-
+from solution import make_negative
 
 @test.describe("Fixed Tests")
-def fixed_tests():
+def basic_tests():
     @test.it('Basic Test Cases')
     def basic_test_cases():
-        test.assert_equals(open_or_senior([(45, 12), (55, 21), (19, -2), (104, 20)]),
-                           ['Open', 'Senior', 'Open', 'Senior'])
-        test.assert_equals(open_or_senior([(16, 23), (73, 1), (56, 20), (1, -1)]), ['Open', 'Open', 'Senior', 'Open'])
+        for n, expected in ((42,-42), (-9,-9), (0,0)):
+            actual = make_negative(n)
+            message = f"For n = {n}, expected {expected} but got {actual}"
+            test.assert_equals(actual, expected, message)
--- solution.py
+++ solution.py
@@ -1,13 +1,2 @@
-def open_or_senior(data):
-    membership = []
-    for member in data:
-        age = member[0]
-        handicap = member[1]
-
-        if age >= 55 and handicap > 7:
-            membership.append("Senior")
-        else:
-            membership.append("Open")
-
-    return membership
-
+def make_negative(number):
+    pass
