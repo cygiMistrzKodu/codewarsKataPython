781cc1c06707952b903a150a81878d93ab7a0a88
Jacek programmer <jacek301@gmail.com>
2024-05-08 22:02:06 +0200

    Tribonacci Sequence Kata Solution

--- solution.py
+++ solution.py
@@ -1,2 +1,12 @@
 def tribonacci(signature, n):
-    pass
+
+    tribonacci_sequence = []
+    for i in range(0, n):
+
+        if i < 3:
+            tribonacci_sequence.append(signature[i])
+        else:
+            next_number = tribonacci_sequence[i - 3] + tribonacci_sequence[i - 2] + tribonacci_sequence[i - 1]
+            tribonacci_sequence.append(next_number)
+
+    return tribonacci_sequence
