c3888006a424f318fc8dbd02f30ab638068b1e97
Jacek programmer <jacek301@gmail.com>
2024-05-27 14:51:58 +0200

    Simple Fun #129: Repeat Sequence Length Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,18 +1,13 @@
 import codewars_test as test
-from solution import xo
+from solution import repeat_sequence_len
 
-@test.describe("Sample tests")
-def _():
-    test_cases = [
-        ("ooxx",    True),
-        ("xooxx",   False),
-        ("ooxXm",   True), # Comparison is case-insensitive
-        ("zpzpzpp", True), # when no 'x' and 'o' is present should return true
-        ("zzoo",    False),
-        ("oxOx",    True),
-        ("",        True),  # Empty string contains equal amount of x and o
-    ]
-    for s, expected in test_cases:
-        @test.it(f"{s = }")
-        def _():
-            test.assert_equals(xo(s), expected)
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it("Basic Test Cases")
+    def basic_test_cases():
+        test.assert_equals(repeat_sequence_len(1), 1)
+        test.assert_equals(repeat_sequence_len(85), 8)
+        test.assert_equals(repeat_sequence_len(810), 8)
+        test.assert_equals(repeat_sequence_len(812), 8)
+        test.assert_equals(repeat_sequence_len(818), 1)
+        test.assert_equals(repeat_sequence_len(833), 1)
--- solution.py
+++ solution.py
@@ -1,4 +1,2 @@
-def xo(s: str):
-    number_of_o = s.lower().count("o")
-    number_of_s = s.lower().count("x")
-    return number_of_o == number_of_s
+def repeat_sequence_len(n):
+    pass
