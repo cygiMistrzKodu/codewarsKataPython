6f41069e15db81c02dbc0e268ff33b5c06592f9a
Jacek programmer <jacek301@gmail.com>
2024-05-27 13:52:18 +0200

    String ends with? Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,21 +1,32 @@
+from solution import solution
 import codewars_test as test
-from solution import tribonacci
+from random import randint
 
+fixed_tests_True = (
+    ( "samurai", "ai"    ),
+    ( "ninja",   "ja"    ),
+    ( "sensei",  "i"     ),
+    ( "abc",     "abc"   ),
+    ( "abcabc",  "bc"    ),
+    ( "fails",   "ails"  ),
+)
 
-@test.describe("Basic Tests")
-def basic_tests():
-    @test.it("Basic Tests")
-    def basic_tests():
-        test.assert_equals(tribonacci([1, 1, 1], 10), [1, 1, 1, 3, 5, 9, 17, 31, 57, 105])
-        test.assert_equals(tribonacci([0, 0, 1], 10), [0, 0, 1, 1, 2, 4, 7, 13, 24, 44])
-        test.assert_equals(tribonacci([0, 1, 1], 10), [0, 1, 1, 2, 4, 7, 13, 24, 44, 81])
-        test.assert_equals(tribonacci([1, 0, 0], 10), [1, 0, 0, 1, 1, 2, 4, 7, 13, 24])
-        test.assert_equals(tribonacci([0, 0, 0], 10), [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        test.assert_equals(tribonacci([1, 2, 3], 10), [1, 2, 3, 6, 11, 20, 37, 68, 125, 230])
-        test.assert_equals(tribonacci([3, 2, 1], 10), [3, 2, 1, 6, 9, 16, 31, 56, 103, 190])
-        test.assert_equals(tribonacci([1, 1, 1], 1), [1])
-        test.assert_equals(tribonacci([300, 200, 100], 0), [])
-        test.assert_equals(tribonacci([0.5, 0.5, 0.5], 30),
-                           [0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5, 1104.5,
-                            2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5, 266079.5, 489396.5,
-                            900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5])
+fixed_tests_False = (
+    ( "sumo",    "omo"   ),
+    ( "samurai", "ra"    ),
+    ( "abc",     "abcd"  ),
+    ( "ails",    "fails" ),
+    ( "this",    "fails" ),
+    ( "spam",    "eggs"  )
+)
+
+@test.describe("Fixed Tests")
+def test_group():
+    @test.it("True Cases")
+    def test_case():
+        for text, ending in fixed_tests_True:
+            test.assert_equals(solution(text, ending), True)
+    @test.it("False Cases")
+    def test_case():
+        for text, ending in fixed_tests_False:
+            test.assert_equals(solution(text, ending), False)
--- solution.py
+++ solution.py
@@ -1,12 +1,3 @@
-def tribonacci(signature, n):
-
-    tribonacci_sequence = []
-    for i in range(0, n):
-
-        if i < 3:
-            tribonacci_sequence.append(signature[i])
-        else:
-            next_number = tribonacci_sequence[i - 3] + tribonacci_sequence[i - 2] + tribonacci_sequence[i - 1]
-            tribonacci_sequence.append(next_number)
-
-    return tribonacci_sequence
+def solution(text, ending):
+    # your code here...
+    pass
