81159421144ace94e33ef64498b9a261cd87a01b
Jacek programmer <jacek301@gmail.com>
2024-05-28 23:08:53 +0200

    Highest and Lowest Kata Solution

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -6,8 +6,8 @@ from solution import high_and_low
 def fixed_tests():
     @test.it('Test Case 1')
     def basic_test_cases():
-        test.assert_equals(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"), "42 -9");
+        test.assert_equals(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"), "42 -9")
 
     @test.it('Test Case 2')
     def basic_test_cases():
-        test.assert_equals(high_and_low("1 2 3"), "3 1");
+        test.assert_equals(high_and_low("1 2 3"), "3 1")
--- solution.py
+++ solution.py
@@ -1,3 +1,3 @@
-def high_and_low(numbers):
-    # ...
-    return numbers
+def high_and_low(numbers: str):
+    numbers_int_list = [int(x) for x in numbers.split(" ")]
+    return str(max(numbers_int_list)) + " " + str(min(numbers_int_list))
