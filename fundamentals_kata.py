854a9544290426e4688c6b4f3a7401078bcf7df6
Jacek programmer <jacek301@gmail.com>
2024-05-28 23:12:42 +0200

    Remove the minimum Kata Start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,13 +1,25 @@
 import codewars_test as test
-from solution import high_and_low
+from solution import remove_smallest
+from numpy.random import randint
 
 
-@test.describe("Example Tests")
+def randlist():
+    return list(randint(400, size=randint(1, 10)))
+
+
+@test.describe("Fixed Tests")
 def fixed_tests():
-    @test.it('Test Case 1')
+    @test.it('Basic Test Cases')
     def basic_test_cases():
-        test.assert_equals(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"), "42 -9")
+        test.assert_equals(remove_smallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]")
+        test.assert_equals(remove_smallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]")
+        test.assert_equals(remove_smallest([1, 2, 3, 1, 1]), [2, 3, 1, 1], "Wrong result for [1, 2, 3, 1, 1]")
+        test.assert_equals(remove_smallest([]), [], "Wrong result for []")
 
-    @test.it('Test Case 2')
-    def basic_test_cases():
-        test.assert_equals(high_and_low("1 2 3"), "3 1")
+    @test.it("check for mutations to original list")
+    def _():
+        a = randlist()  # generates the list
+        b = list(a)  # makes a swallow copy
+        remove_smallest(a)  # uses the original list with the function
+        test.assert_equals(a, b,
+                           "You've mutated input list  (expectation assertion is on value of input list, not output of method)")  # test the list match
--- solution.py
+++ solution.py
@@ -1,3 +1,2 @@
-def high_and_low(numbers: str):
-    numbers_int_list = [int(x) for x in numbers.split(" ")]
-    return str(max(numbers_int_list)) + " " + str(min(numbers_int_list))
+def remove_smallest(numbers):
+    return []
