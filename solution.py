a72c31f43d70ec4bbb2130c858fd6e38b802c887
Jacek programmer <jacek301@gmail.com>
2024-05-28 22:45:05 +0200

    Transportation on vacation Kata Stop

--- solution.py
+++ solution.py
@@ -1,2 +1,11 @@
 def rental_car_cost(d):
-    pass
+    normal_rental_cost = d * 40
+
+    if 3 < d < 7:
+        return normal_rental_cost - 20
+
+    if d < 7:
+        return normal_rental_cost
+
+    if d >= 7:
+        return normal_rental_cost - 50
