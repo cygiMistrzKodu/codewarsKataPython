0bb17f28dc8a9ecc7410ea81210a6c673e5cfb05
Jacek programmer <jacek301@gmail.com>
2024-05-28 23:36:45 +0200

    Remove the minimum Kata solution

--- solution.py
+++ solution.py
@@ -1,2 +1,12 @@
-def remove_smallest(numbers):
-    return []
+def remove_smallest(numbers: list[int]):
+    if not numbers:
+        return []
+
+    smallest_rating = min(numbers)
+    exhibits_without_lowest_rating = numbers.copy()
+    try:
+        exhibits_without_lowest_rating.remove(smallest_rating)
+    except ValueError:
+        return []
+
+    return exhibits_without_lowest_rating
