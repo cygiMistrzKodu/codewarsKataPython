4bc5f2e1eb1c7792c2911955062a0b77fed28aac
Jacek programmer <jacek301@gmail.com>
2024-05-28 23:40:15 +0200

    Removing Elements Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,25 +1,14 @@
 import codewars_test as test
-from solution import remove_smallest
-from numpy.random import randint
-
-
-def randlist():
-    return list(randint(400, size=randint(1, 10)))
-
+from solution import remove_every_other
 
 @test.describe("Fixed Tests")
 def fixed_tests():
     @test.it('Basic Test Cases')
     def basic_test_cases():
-        test.assert_equals(remove_smallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]")
-        test.assert_equals(remove_smallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]")
-        test.assert_equals(remove_smallest([1, 2, 3, 1, 1]), [2, 3, 1, 1], "Wrong result for [1, 2, 3, 1, 1]")
-        test.assert_equals(remove_smallest([]), [], "Wrong result for []")
-
-    @test.it("check for mutations to original list")
-    def _():
-        a = randlist()  # generates the list
-        b = list(a)  # makes a swallow copy
-        remove_smallest(a)  # uses the original list with the function
-        test.assert_equals(a, b,
-                           "You've mutated input list  (expectation assertion is on value of input list, not output of method)")  # test the list match
+        test.assert_equals(remove_every_other(['Hello', 'Goodbye', 'Hello Again']),
+                           ['Hello', 'Hello Again'])
+        test.assert_equals(remove_every_other([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+                           [1, 3, 5, 7, 9])
+        test.assert_equals(remove_every_other([[1, 2]]), [[1, 2]])
+        test.assert_equals(remove_every_other([['Goodbye'], {'Great': 'Job'}]),
+                           [['Goodbye']])
--- solution.py
+++ solution.py
@@ -1,12 +1,2 @@
-def remove_smallest(numbers: list[int]):
-    if not numbers:
-        return []
-
-    smallest_rating = min(numbers)
-    exhibits_without_lowest_rating = numbers.copy()
-    try:
-        exhibits_without_lowest_rating.remove(smallest_rating)
-    except ValueError:
-        return []
-
-    return exhibits_without_lowest_rating
+def remove_every_other(my_list):
+    pass
