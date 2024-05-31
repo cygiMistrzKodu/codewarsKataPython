e6c0ff6981e215c4ccef29a0b928388bc0a5f664
Jacek programmer <jacek301@gmail.com>
2024-05-31 16:00:03 +0200

    Reversed Strings Kata start

--- solution.py
+++ solution.py
@@ -1,10 +1,2 @@
-import math
-
-
-def century(year):
-    century_in = year / 100
-
-    if century_in.is_integer():
-        return century_in
-    else:
-        return math.floor(century_in) + 1
+def solution(string):
+    pass
