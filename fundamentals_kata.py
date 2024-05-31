de5b779dfb18fdd7ccc88b30f2b650385c9c7cb8
Jacek programmer <jacek301@gmail.com>
2024-05-31 14:26:41 +0200

    Century From Year Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,10 +1,14 @@
 import codewars_test as test
-from solution import get_volume_of_cuboid
+from solution import century
 
 
-@test.describe('Testing...')
-def _():
-    @test.it('Sample tests')
-    def _():
-        test.assert_equals(get_volume_of_cuboid(1, 2, 2), 4)
-        test.assert_equals(get_volume_of_cuboid(6.3, 2, 5), 63)
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(century(1705), 18, 'Testing for year 1705')
+        test.assert_equals(century(1900), 19, 'Testing for year 1900')
+        test.assert_equals(century(1601), 17, 'Testing for year 1601')
+        test.assert_equals(century(2000), 20, 'Testing for year 2000')
+        test.assert_equals(century(356), 4, 'Testing for year 356')
+        test.assert_equals(century(89), 1, 'Testing for year 89')
--- solution.py
+++ solution.py
@@ -1,2 +1,3 @@
-def get_volume_of_cuboid(length, width, height):
-    return length * width * height
+def century(year):
+    # Finish this :)
+    return
