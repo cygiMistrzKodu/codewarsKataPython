75ca1ece72d0c28582d6a2f4152d7b86834818b9
Jacek programmer <jacek301@gmail.com>
2024-05-31 14:37:42 +0200

    Century From Year Kata solution

--- solution.py
+++ solution.py
@@ -1,3 +1,10 @@
+import math
+
+
 def century(year):
-    # Finish this :)
-    return
+    century_in = year / 100
+
+    if century_in.is_integer():
+        return century_in
+    else:
+        return math.floor(century_in) + 1
