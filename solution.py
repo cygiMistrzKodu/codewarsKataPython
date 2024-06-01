aa2d8a06a8e9e258c413a98030c9f76f8b009a38
Jacek programmer <jacek301@gmail.com>
2024-06-01 15:34:07 +0200

    Disemvowel Trolls Kata start

--- solution.py
+++ solution.py
@@ -1,6 +1,2 @@
-def zero_fuel(distance_to_pump, mpg, fuel_left):
-    miles_will_go = fuel_left * mpg
-    if distance_to_pump <= miles_will_go:
-        return True
-
-    return False
+def disemvowel(string_):
+    return string_
