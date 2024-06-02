caeec73a79f4482c30b8a5e38756dde2adeb08a3
Jacek programmer <jacek301@gmail.com>
2024-06-02 14:51:31 +0200

    Fake Binary Kata solution

--- solution.py
+++ solution.py
@@ -1,2 +1,11 @@
-def fake_bin(x):
-    pass
+def fake_bin(x: str):
+    binary_string = []
+    for digit in x:
+        integer_digit = int(digit)
+
+        if integer_digit < 5:
+            binary_string.append("0")
+        else:
+            binary_string.append("1")
+
+    return "".join(binary_string)
