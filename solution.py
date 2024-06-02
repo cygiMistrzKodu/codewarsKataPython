7bb5fa02192a7d29f6c747df961e1450bd4b3364
Jacek programmer <jacek301@gmail.com>
2024-06-02 15:30:39 +0200

    Is this a triangle? Kata solution

--- solution.py
+++ solution.py
@@ -1,2 +1,12 @@
 def is_triangle(a, b, c):
-    return False
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return False
+
+    possible_triangle_length = [a, b, c]
+    longest_side = max(possible_triangle_length)
+    del possible_triangle_length[possible_triangle_length.index(longest_side)]
+
+    sum_of_two_shorter_sides = sum(possible_triangle_length)
+
+    return longest_side < sum_of_two_shorter_sides
