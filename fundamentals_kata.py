b869f8a92eb5e80ad953f822615072705a79f5a9
Jacek programmer <jacek301@gmail.com>
2024-06-03 18:45:17 +0200

    Student's Final Grade Kata solution

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,9 +1,14 @@
 import codewars_test as test
 from solution import final_grade
 
+
 @test.describe("Fixed Tests")
 def fixed_tests():
     @test.it('Basic Test Cases')
     def basic_test_cases():
         test.assert_equals(final_grade(100, 12), 100)
         test.assert_equals(final_grade(85, 5), 90)
+        test.assert_equals(final_grade(99, 0), 100)
+        test.assert_equals(final_grade(55, 3), 75)
+        test.assert_equals(final_grade(55, 0), 0)
+        test.assert_equals(final_grade(20, 2), 0)
--- solution.py
+++ solution.py
@@ -1,2 +1,10 @@
 def final_grade(exam, projects):
-    return # final grade
+    match (exam, projects):
+        case (e, p) if e > 90 or p > 10:
+            return 100
+        case (e, p) if e > 75 and p >= 5:
+            return 90
+        case (e, p) if e > 50 and p >= 2:
+            return 75
+        case _:
+            return 0
