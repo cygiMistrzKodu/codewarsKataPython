6343eb214558f412dffe8d60ae72097253d1f1db
Jacek programmer <jacek301@gmail.com>
2024-06-03 23:52:20 +0200

    Sum without highest and lowest number Kata solution

--- solution.py
+++ solution.py
@@ -1,2 +1,11 @@
-def sum_array(arr):
-    pass
+def sum_array(arr: list[int]):
+    if not arr:
+        return 0
+
+    if len(arr) == 1:
+        return 0
+
+    arr.remove(min(arr))
+    arr.remove(max(arr))
+
+    return sum(arr)
