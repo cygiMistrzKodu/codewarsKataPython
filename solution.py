df7b54115beb97a44d534757fea5e988820144a6
Jacek programmer <jacek301@gmail.com>
2024-06-03 21:01:35 +0200

    Get the Middle Character Kata solution

--- solution.py
+++ solution.py
@@ -1,2 +1,9 @@
 def get_middle(s):
-    pass
+    word_length = len(s)
+
+    if word_length % 2 == 0:
+        second_letter = int(word_length / 2)
+        first_letter = second_letter - 1
+        return s[first_letter] + s[second_letter]
+    else:
+        return s[int((word_length + 1) / 2) - 1]
