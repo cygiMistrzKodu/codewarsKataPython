8c1bfddbaea6ca181b9f880b34376d75d1819530
Jacek programmer <jacek301@gmail.com>
2024-06-03 18:50:24 +0200

    Is it even? Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,14 +1,18 @@
 import codewars_test as test
-from solution import final_grade
+from solution import is_even
 
 
-@test.describe("Fixed Tests")
+@test.describe('Fixed Tests')
 def fixed_tests():
     @test.it('Basic Test Cases')
-    def basic_test_cases():
-        test.assert_equals(final_grade(100, 12), 100)
-        test.assert_equals(final_grade(85, 5), 90)
-        test.assert_equals(final_grade(99, 0), 100)
-        test.assert_equals(final_grade(55, 3), 75)
-        test.assert_equals(final_grade(55, 0), 0)
-        test.assert_equals(final_grade(20, 2), 0)
+    def basic_tests():
+        test.assert_equals(is_even(0), True)
+        test.assert_equals(is_even(0.5), False)
+        test.assert_equals(is_even(1), False)
+        test.assert_equals(is_even(2), True)
+        test.assert_equals(is_even(-4), True)
+        test.assert_equals(is_even(15), False)
+        test.assert_equals(is_even(20), True)
+        test.assert_equals(is_even(220), True)
+        test.assert_equals(is_even(222222221), False)
+        test.assert_equals(is_even(500000000), True)
--- solution.py
+++ solution.py
@@ -1,10 +1,3 @@
-def final_grade(exam, projects):
-    match (exam, projects):
-        case (e, p) if e > 90 or p > 10:
-            return 100
-        case (e, p) if e > 75 and p >= 5:
-            return 90
-        case (e, p) if e > 50 and p >= 2:
-            return 75
-        case _:
-            return 0
+def is_even(n):
+    # your code here
+    pass
