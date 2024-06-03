0d733d412b9f4685463e32d024962e76c95f0201
Jacek programmer <jacek301@gmail.com>
2024-06-03 21:05:29 +0200

    Sum without highest and lowest number Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,13 +1,27 @@
 import codewars_test as test
-from solution import get_middle
+from solution import sum_array
 
 
-@test.describe("Basic Tests")
-def basic_tests():
-    @test.it("Basic Tests")
-    def basic_tests():
-        test.assert_equals(get_middle("test"), "es")
-        test.assert_equals(get_middle("testing"), "t")
-        test.assert_equals(get_middle("middle"), "dd")
-        test.assert_equals(get_middle("A"), "A")
-        test.assert_equals(get_middle("of"), "of")
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('None or Empty')
+    def basic_test_cases():
+        test.assert_equals(sum_array(None), 0)
+        test.assert_equals(sum_array([]), 0)
+
+    @test.it("Only one Element")
+    def one_test_cases():
+        test.assert_equals(sum_array([3]), 0)
+        test.assert_equals(sum_array([-3]), 0)
+
+    @test.it("Only two Element")
+    def two_test_cases():
+        test.assert_equals(sum_array([3, 5]), 0)
+        test.assert_equals(sum_array([-3, -5]), 0)
+
+    @test.it("Real Tests")
+    def real_test_cases():
+        test.assert_equals(sum_array([6, 2, 1, 8, 10]), 16)
+        test.assert_equals(sum_array([6, 0, 1, 10, 10]), 17)
+        test.assert_equals(sum_array([-6, -20, -1, -10, -12]), -28)
+        test.assert_equals(sum_array([-6, 20, -1, 10, -12]), 3)
--- solution.py
+++ solution.py
@@ -1,9 +1,2 @@
-def get_middle(s):
-    word_length = len(s)
-
-    if word_length % 2 == 0:
-        second_letter = int(word_length / 2)
-        first_letter = second_letter - 1
-        return s[first_letter] + s[second_letter]
-    else:
-        return s[int((word_length + 1) / 2) - 1]
+def sum_array(arr):
+    pass
