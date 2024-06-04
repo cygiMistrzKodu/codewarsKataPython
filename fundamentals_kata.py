2af654c0ac44f82822a42f5a522c56ebec0c4285
Jacek programmer <jacek301@gmail.com>
2024-06-04 17:48:51 +0200

    love you, a little , a lot, passionately ... not at all Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,17 +1,10 @@
 import codewars_test as test
-from solution import sort_array
-
+from solution import how_much_i_love_you
 
 @test.describe("Fixed Tests")
 def fixed_tests():
     @test.it('Basic Test Cases')
     def basic_test_cases():
-        test.assert_equals(sort_array([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
-        test.assert_equals(sort_array([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
-        test.assert_equals(sort_array([]), [])
-        test.assert_equals(sort_array([5, 3, 2, 8, 1, 4, 11]), [1, 3, 2, 8, 5, 4, 11])
-        test.assert_equals(sort_array([2, 22, 37, 11, 4, 1, 5, 0]), [2, 22, 1, 5, 4, 11, 37, 0])
-        test.assert_equals(sort_array([1, 111, 11, 11, 2, 1, 5, 0]), [1, 1, 5, 11, 2, 11, 111, 0])
-        test.assert_equals(sort_array([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
-        test.assert_equals(sort_array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        test.assert_equals(sort_array([0, 1, 2, 3, 4, 9, 8, 7, 6, 5]), [0, 1, 2, 3, 4, 5, 8, 7, 6, 9])
+        test.assert_equals(how_much_i_love_you(7),"I love you")
+        test.assert_equals(how_much_i_love_you(3),"a lot")
+        test.assert_equals(how_much_i_love_you(6),"not at all")
--- solution.py
+++ solution.py
@@ -1,3 +1,2 @@
-def sort_array(source_array: list[int]):
-    odds = sorted((x for x in source_array if x % 2 != 0), reverse=True)
-    return [x if x % 2 == 0 else odds.pop() for x in source_array]
+def how_much_i_love_you(nb_petals):
+    pass
