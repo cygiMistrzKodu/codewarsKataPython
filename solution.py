dca7c12e21309a42201bd099a38174a823fee732
Jacek programmer <jacek301@gmail.com>
2024-06-04 17:44:06 +0200

    Sort the odd Kata other solution

--- solution.py
+++ solution.py
@@ -1,21 +1,3 @@
-class OddNumberSorter:
-    def __init__(self, odd_number_list):
-        self.odd_number_list = odd_number_list
-        self.odd_number_list.sort()
-
-    next_odd_number_index = 0
-
-    def next_number(self):
-        odd_number = self.odd_number_list[self.next_odd_number_index]
-        self.next_odd_number_index += 1
-        return odd_number
-
-
 def sort_array(source_array: list[int]):
-    only_odd_numbers = [number for number in source_array if number % 2 != 0]
-
-    odd_number_sorter = OddNumberSorter(only_odd_numbers)
-
-    sorted_odd_numbers = [number if (number % 2 == 0) else odd_number_sorter.next_number() for number in source_array]
-
-    return sorted_odd_numbers
+    odds = sorted((x for x in source_array if x % 2 != 0), reverse=True)
+    return [x if x % 2 == 0 else odds.pop() for x in source_array]
