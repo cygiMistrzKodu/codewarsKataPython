b6fa2729fa1d62c2e7e07de86094826967d21923
Jacek programmer <jacek301@gmail.com>
2024-06-04 13:46:28 +0200

    String repeat Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,27 +1,11 @@
 import codewars_test as test
-from solution import sum_array
+from solution import repeat_str
 
 
-@test.describe("Fixed Tests")
-def fixed_tests():
-    @test.it('None or Empty')
+@test.describe('Fixed tests')
+def basic_tests():
+    @test.it('Basic Test Cases')
     def basic_test_cases():
-        test.assert_equals(sum_array(None), 0)
-        test.assert_equals(sum_array([]), 0)
-
-    @test.it("Only one Element")
-    def one_test_cases():
-        test.assert_equals(sum_array([3]), 0)
-        test.assert_equals(sum_array([-3]), 0)
-
-    @test.it("Only two Element")
-    def two_test_cases():
-        test.assert_equals(sum_array([3, 5]), 0)
-        test.assert_equals(sum_array([-3, -5]), 0)
-
-    @test.it("Real Tests")
-    def real_test_cases():
-        test.assert_equals(sum_array([6, 2, 1, 8, 10]), 16)
-        test.assert_equals(sum_array([6, 0, 1, 10, 10]), 17)
-        test.assert_equals(sum_array([-6, -20, -1, -10, -12]), -28)
-        test.assert_equals(sum_array([-6, 20, -1, 10, -12]), 3)
+        test.assert_equals(repeat_str(4, 'a'), 'aaaa')
+        test.assert_equals(repeat_str(3, 'hello '), 'hello hello hello ')
+        test.assert_equals(repeat_str(2, 'abc'), 'abcabc')
--- solution.py
+++ solution.py
@@ -1,11 +1,2 @@
-def sum_array(arr: list[int]):
-    if not arr:
-        return 0
-
-    if len(arr) == 1:
-        return 0
-
-    arr.remove(min(arr))
-    arr.remove(max(arr))
-
-    return sum(arr)
+def repeat_str(repeat, string):
+    return ''
