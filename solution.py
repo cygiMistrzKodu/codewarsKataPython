6b6787d4be8abd683cc075fb7bed252912e18fdb
Jacek programmer <jacek301@gmail.com>
2024-06-05 11:46:26 +0200

    Sum The Strings Kata my solution

--- solution.py
+++ solution.py
@@ -1,3 +1,11 @@
-def sum_str(a, b):
-    # happy coding !
-    pass
+def sum_str(a: str, b: str):
+    if a == "" and b == "":
+        return "0"
+
+    if a == "" and len(b) > 0:
+        return b
+
+    if len(a) > 0 and b == "":
+        return a
+
+    return "".join(str(int(a) + int(b)))
