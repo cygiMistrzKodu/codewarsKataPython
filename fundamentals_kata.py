3d78804906b61a45161668f8125b494c37b8d592
Jacek programmer <jacek301@gmail.com>
2024-06-05 10:27:47 +0200

    love you, a little , a lot, passionately ... not at all Kata solution

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,10 +1,11 @@
 import codewars_test as test
 from solution import how_much_i_love_you
 
+
 @test.describe("Fixed Tests")
 def fixed_tests():
     @test.it('Basic Test Cases')
     def basic_test_cases():
-        test.assert_equals(how_much_i_love_you(7),"I love you")
-        test.assert_equals(how_much_i_love_you(3),"a lot")
-        test.assert_equals(how_much_i_love_you(6),"not at all")
+        test.assert_equals(how_much_i_love_you(7), "I love you")
+        test.assert_equals(how_much_i_love_you(3), "a lot")
+        test.assert_equals(how_much_i_love_you(6), "not at all")
--- solution.py
+++ solution.py
@@ -1,2 +1,11 @@
 def how_much_i_love_you(nb_petals):
-    pass
+    petals_phrase = {
+        1: "I love you",
+        2: "a little",
+        3: "a lot",
+        4: "passionately",
+        5: "madly",
+        0: "not at all",
+    }
+
+    return petals_phrase[nb_petals % 6]
