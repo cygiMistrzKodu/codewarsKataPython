ace692542a1aa460fe646aac37eada22b9744e96
Jacek programmer <jacek301@gmail.com>
2024-06-07 17:48:04 +0200

    Testing 1-2-3 Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,16 +1,10 @@
 import codewars_test as test
-from solution import grille
+from solution import number
 
 
 @test.describe("Fixed Tests")
 def fixed_tests():
     @test.it('Basic Test Cases')
     def basic_test_cases():
-        test.assert_equals(grille("abcdef", 5), "df")
-        test.assert_equals(grille("", 5), "")
-        test.assert_equals(grille("abcd", 1), "d")
-        test.assert_equals(grille("0abc", 2), "b")
-        test.assert_equals(grille("ab", 255), "ab")
-        test.assert_equals(grille("ab", 256), "")
-        test.assert_equals(grille("abcde", 32), "")
-        test.assert_equals(grille("tcddoadepwweasresd", 77098), "codewars")
+        test.assert_equals(number([]), [])
+        test.assert_equals(number(["a", "b", "c"]), ["1: a", "2: b", "3: c"])
