8345f1f5ffdc334d7ceb204add1f31f2d8d270b0
Jacek programmer <jacek301@gmail.com>
2024-06-08 13:09:10 +0200

    Calculate BMI Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,37 +1,13 @@
 import codewars_test as test
-from solution import get_grade
+from solution import bmi
 
 
-@test.describe("Grade book")
+@test.describe("Fixed Tests")
 def fixed_tests():
-    @test.it('should return an A')
-    def a_test_case():
-        test.assert_equals(get_grade(95, 90, 93), "A", "get_grade(95, 90, 93)")
-        test.assert_equals(get_grade(100, 85, 96), "A", "get_grade(100, 85, 96)")
-        test.assert_equals(get_grade(92, 93, 94), "A", "get_grade(92, 93, 94)")
-        test.assert_equals(get_grade(100, 100, 100), "A", "get_grade(100, 100, 100)")
-
-    @test.it("should return a B")
-    def b_test_case():
-        test.assert_equals(get_grade(70, 70, 100), "B", "get_grade(70, 70, 100)")
-        test.assert_equals(get_grade(82, 85, 87), "B", "get_grade(82, 85, 87)")
-        test.assert_equals(get_grade(84, 79, 85), "B", "get_grade(84, 79, 85)")
-
-    @test.it("should return a C")
-    def c_test_case():
-        test.assert_equals(get_grade(70, 70, 70), "C", "get_grade(70, 70, 70)")
-        test.assert_equals(get_grade(75, 70, 79), "C", "get_grade(75, 70, 79)")
-        test.assert_equals(get_grade(60, 82, 76), "C", "get_grade(60, 82, 76)")
-
-    @test.it("should return a D")
-    def d_test_case():
-        test.assert_equals(get_grade(65, 70, 59), "D", "get_grade(65, 70, 59)")
-        test.assert_equals(get_grade(66, 62, 68), "D", "get_grade(66, 62, 68)")
-        test.assert_equals(get_grade(58, 62, 70), "D", "get_grade(58, 62, 70)")
-
-    @test.it("should return an F")
-    def f_test_case():
-        test.assert_equals(get_grade(44, 55, 52), "F", "get_grade(44, 55, 52)")
-        test.assert_equals(get_grade(48, 55, 52), "F", "get_grade(48, 55, 52)")
-        test.assert_equals(get_grade(58, 59, 60), "F", "get_grade(58, 59, 60)")
-        test.assert_equals(get_grade(0, 0, 0), "F", "get_grade(0, 0, 0)")
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(bmi(50, 1.80), "Underweight")
+        test.assert_equals(bmi(80, 1.80), "Normal")
+        test.assert_equals(bmi(90, 1.80), "Overweight")
+        test.assert_equals(bmi(110, 1.80), "Obese")
+        test.assert_equals(bmi(50, 1.50), "Normal")
--- solution.py
+++ solution.py
@@ -1,20 +1,3 @@
-import statistics
-
-
-def get_grade(s1, s2, s3):
-    mean_scores = statistics.mean([s1, s2, s3])
-
-    if 90 <= mean_scores <= 100:
-        return "A"
-
-    if 80 <= mean_scores < 90:
-        return "B"
-
-    if 70 <= mean_scores < 80:
-        return "C"
-
-    if 60 <= mean_scores < 70:
-        return "D"
-
-    if 0 <= mean_scores < 60:
-        return "F"
+def bmi(weight, height):
+    #your code here
+    pass
