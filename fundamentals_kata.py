4c5f9479d092e75f91a4da7a39ac4973c219250b
Jacek programmer <jacek301@gmail.com>
2024-06-08 13:21:29 +0200

    Difference of Volumes of Cuboids Kata start

--- fundamentals_kata.py
+++ fundamentals_kata.py
@@ -1,13 +1,12 @@
 import codewars_test as test
-from solution import bmi
+from solution import find_difference
 
 
 @test.describe("Fixed Tests")
 def fixed_tests():
     @test.it('Basic Test Cases')
     def basic_test_cases():
-        test.assert_equals(bmi(50, 1.80), "Underweight")
-        test.assert_equals(bmi(80, 1.80), "Normal")
-        test.assert_equals(bmi(90, 1.80), "Overweight")
-        test.assert_equals(bmi(110, 1.80), "Obese")
-        test.assert_equals(bmi(50, 1.50), "Normal")
+        test.assert_equals(find_difference([3, 2, 5], [1, 4, 4]), 14,
+                           "{0} should equal 14".format(find_difference([3, 2, 5], [1, 4, 4])))
+        test.assert_equals(find_difference([9, 7, 2], [5, 2, 2]), 106,
+                           "{0} should equal 106".format(find_difference([9, 7, 2], [5, 2, 2])))
--- solution.py
+++ solution.py
@@ -1,13 +1,2 @@
-def bmi(weight, height):
-    bmi_index = weight / (height * height)
-
-    if bmi_index <= 18.5:
-        return "Underweight"
-
-    if bmi_index <= 25.0:
-        return "Normal"
-
-    if bmi_index <= 30:
-        return "Overweight"
-
-    return "Obese"
+def find_difference(a, b):
+    pass
