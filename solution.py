e4be5c9915daa477152a19ad546e38f94cf98141
Jacek programmer <jacek301@gmail.com>
2024-06-08 12:20:04 +0200

    Grasshopper - Grade book Kata solution

--- solution.py
+++ solution.py
@@ -1,3 +1,20 @@
+import statistics
+
+
 def get_grade(s1, s2, s3):
-    # Code here
-    return "F"
+    mean_scores = statistics.mean([s1, s2, s3])
+
+    if 90 <= mean_scores <= 100:
+        return "A"
+
+    if 80 <= mean_scores < 90:
+        return "B"
+
+    if 70 <= mean_scores < 80:
+        return "C"
+
+    if 60 <= mean_scores < 70:
+        return "D"
+
+    if 0 <= mean_scores < 60:
+        return "F"
