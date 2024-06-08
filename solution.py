dc05ff4ce292cdf88e7281e5f456958b7fabbee7
Jacek programmer <jacek301@gmail.com>
2024-06-08 13:43:16 +0200

    Difference of Volumes of Cuboids Kata solution

--- solution.py
+++ solution.py
@@ -1,2 +1,13 @@
-def find_difference(a, b):
-    pass
+from functools import reduce
+
+
+def find_difference(a: list[int], b: list[int]):
+    volume_a = reduce((lambda x, y: x * y), a)
+    volume_b = reduce((lambda x, y: x * y), b)
+
+    difference = volume_a - volume_b
+
+    if difference < 0:
+        return difference * -1
+    else:
+        return difference
