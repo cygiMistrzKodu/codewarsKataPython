25f154992b4a274279aec18f1e7c373d5d4e8f8c
Jacek programmer <jacek301@gmail.com>
2024-06-08 13:18:07 +0200

    Calculate BMI Kata solution

--- solution.py
+++ solution.py
@@ -1,3 +1,13 @@
 def bmi(weight, height):
-    #your code here
-    pass
+    bmi_index = weight / (height * height)
+
+    if bmi_index <= 18.5:
+        return "Underweight"
+
+    if bmi_index <= 25.0:
+        return "Normal"
+
+    if bmi_index <= 30:
+        return "Overweight"
+
+    return "Obese"
